36811fe437b6bd5c4d9ad496defcced7858356e5
abhishekjha9909 <abhishekjha9909@gmail.com>
2024-10-13 22:04:01 +0530

    New Changes

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,8 +1,56 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 class Main{
-    public static void main(String[] args){
+
+
+//    public static void main(String[] args){
+//        System.out.println("Hello");
+//
+//        for(int i=0;i<10;++i) {
+//            System.out.println("Hello");
+//            String s=new String();
+//        }
+//    }
+
+
+    /**
+     * @param obj 
+     * @return
+     */
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    /**
+     * @return 
+     */
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    /**
+     * 
+     */
+    public Main() {
+        super();
+    }
+
+    public static void method1(){
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
         System.out.println("Hello");
     }
+
+    public void method2(){
+//        method1();
+        List<Integer> lis=new ArrayList<>();
+
+        lis.add
+    }
+
 }
