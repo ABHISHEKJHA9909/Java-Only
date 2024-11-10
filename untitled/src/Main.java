7a84758470e3e70e756d6749767c448555c1eacc
abhishekjha9909 <abhishekjha9909@gmail.com>
2024-11-10 20:26:28 +0530

    New File Added Hello.java

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,15 +1,19 @@
-class Main{
+class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello");
 
-        for(int i=0;i<10;++i) {
-            String s=new String();
+        for (int i = 0; i < 10; ++i) {
+            String s = new String();
         }
 
-        Testing testing=new Testing();
-        testing.test1();
-        testing.test2();
-    }
+        System.out.println("Hello");
 
+        try {
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
